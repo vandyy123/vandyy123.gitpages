67e105592ad19e84fec7f3d1d2a8d70feb694096
vandyy123 <52757945+vandyy123@users.noreply.github.com>
2020-08-04 20:49:58 -0400

    Add files via upload
    Website

---
+++ JavaS.js
@@ -0,0 +1,10 @@
+var firstName= 'John'
+var age= 15
+
+if (age <13) {
+	console.log(firstName + ' is a boy.')
+} else if (13 <= age <= 17) {
+	console.log(firstName + ' is a teenager.')
+} else {
+	console.log(firstName + ' is a man.')
+}
